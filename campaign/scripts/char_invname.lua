465f8b72d04bd0f852d734f634ef1d37435069e9
Wil Thieme <wil.thieme@protonmail.com>
2022-08-15 08:32:12 -0400

    change to double click to collapse

--- campaign/scripts/char_invname.lua
+++ campaign/scripts/char_invname.lua
@@ -2,21 +2,20 @@
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
 
--- luacheck: globals onClickRelease onLoseFocus
+-- luacheck: globals onDoubleClick onLoseFocus
 
-function onClickRelease()
-	if Input.isControlPressed() then
-		local nodeItem = window.getDatabaseNode()
-		if DB.getValue(nodeItem, 'collapsedcontainer') == 'true' then
-			DB.setValue(nodeItem, 'collapsedcontainer', 'string', 'false');
-			DB.setValue(nodeItem, 'name', 'string', '[+] ' .. DB.getValue(nodeItem, 'name', ''));
-			DB.setValue(nodeItem, 'nonid_name', 'string', '[+] ' .. DB.getValue(nodeItem, 'name', ''));
-		else
-			DB.setValue(nodeItem, 'collapsedcontainer', 'string', 'true');
-			DB.setValue(nodeItem, 'name', 'string', DB.getValue(nodeItem, 'name', ''):gsub('%[%+%] ', ''));
-			DB.setValue(nodeItem, 'nonid_name', 'string', DB.getValue(nodeItem, 'name', ''):gsub('%[%+%] ', ''));
-		end
+function onDoubleClick()
+	local nodeItem = window.getDatabaseNode()
+	if DB.getValue(nodeItem, 'collapsedcontainer') == 'true' then
+		DB.setValue(nodeItem, 'collapsedcontainer', 'string', 'false');
+		DB.setValue(nodeItem, 'name', 'string', '[+] ' .. DB.getValue(nodeItem, 'name', ''));
+		DB.setValue(nodeItem, 'nonid_name', 'string', '[+] ' .. DB.getValue(nodeItem, 'name', ''));
+	else
+		DB.setValue(nodeItem, 'collapsedcontainer', 'string', 'true');
+		DB.setValue(nodeItem, 'name', 'string', DB.getValue(nodeItem, 'name', ''):gsub('%[%+%] ', ''));
+		DB.setValue(nodeItem, 'nonid_name', 'string', DB.getValue(nodeItem, 'name', ''):gsub('%[%+%] ', ''));
 	end
+	return true
 end
 
 function onLoseFocus()
