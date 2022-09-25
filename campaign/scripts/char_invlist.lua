4d94fcfc68a7bf5d343fcd7905d14da964e42c50
Wil Thieme <wil.thieme@protonmail.com>
2022-09-25 08:47:08 -0400

    hide containers in other containers (but only one level deep)

--- campaign/scripts/char_invlist.lua
+++ campaign/scripts/char_invlist.lua
@@ -4,13 +4,20 @@
 
 -- luacheck: globals onFilter
 
+-- function is run on each inventory item. return true = visible, return false = hidden.
 function onFilter(w)
 	local nodeItem = w.getDatabaseNode();
 	local _, sLocNode = DB.getValue(nodeItem, 'locationshortcut');
 	if sLocNode then
 		local nodeContainer = DB.findNode(sLocNode);
+		local _, sContainerLocNode = DB.getValue(nodeContainer, 'locationshortcut');
 		if DB.getValue(nodeContainer, 'name', ''):match('%[%+%]%s+') then
 			return false;
+		elseif sContainerLocNode then -- if container is inside another container (max one level deep).
+			local nodeContainerContainer = DB.findNode(sContainerLocNode);
+			if DB.getValue(nodeContainerContainer, 'name', ''):match('%[%+%]%s+') then
+				return false;
+			end
 		end
 	end
 	return true;
