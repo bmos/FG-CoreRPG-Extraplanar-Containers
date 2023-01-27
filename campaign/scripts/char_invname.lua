d8582506a27cddca12be80c4828f66e88df306ac
Wil Thieme <wil.thieme@protonmail.com>
2023-01-26 22:31:15 -0500

    add color and tooltip to weight of overfull containers

--- campaign/scripts/char_invname.lua
+++ campaign/scripts/char_invname.lua
@@ -24,3 +24,37 @@ function onLoseFocus()
 	super.onLoseFocus()
 	window.windowlist.updateContainers()
 end
+
+-- everything below here is responsible for setting the weight to red if the container is overfull
+
+local tTooltips = { ['announcedW'] = 'weight', ['announcedV'] = 'volume' }
+
+local function onAnnounced(_, child)
+	local sNodeName = DB.getName(child)
+	if sNodeName == 'announcedW' or sNodeName == 'announcedV' then
+		window.weight.setColor(ColorManager.COLOR_HEALTH_CRIT_WOUNDS)
+		window.weight.setTooltipText(string.format(Interface.getString('item_tooltip_overfull'), tTooltips[sNodeName]))
+	end
+end
+
+local function onUnannounced(source)
+	local sNodeName = DB.getName(source)
+	if sNodeName == 'announcedW' or sNodeName == 'announcedV' then
+		window.weight.setColor(ColorManager.COLOR_FULL)
+		window.weight.setTooltipText('')
+	end
+end
+
+function onInit()
+	local nodeWeightAnnounced = DB.getChild(window.getDatabaseNode(), 'announcedW')
+	if nodeWeightAnnounced then onAnnounced(nil, nodeWeightAnnounced) end
+	if super and super.onInit then super.onInit() end
+	DB.addHandler(DB.getPath(window.getDatabaseNode()), 'onChildAdded', onAnnounced)
+	DB.addHandler(DB.getPath(window.getDatabaseNode()) .. '.*', 'onDelete', onUnannounced)
+end
+
+function onClose()
+	if super and super.onClose then super.onClose() end
+	DB.removeHandler(DB.getPath(window.getDatabaseNode()), 'onChildAdded', onAnnounced)
+	DB.removeHandler(DB.getPath(window.getDatabaseNode()) .. '.*', 'onDelete', onUnannounced)
+end
