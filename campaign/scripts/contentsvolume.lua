fa4b6a6da3c90631ce024759b985a45371bdb293
Wil Thieme <wil.thieme@protonmail.com>
2023-02-07 10:10:55 -0500

    fix script error when not using volume

--- campaign/scripts/contentsvolume.lua
+++ campaign/scripts/contentsvolume.lua
@@ -13,7 +13,6 @@ function onInit()
 
 	bVisible = bVisible and ExtraplanarContainers.isAnyContainer(DB.getValue(window.getDatabaseNode(), 'name'))
 
-	if not bVisible then window.capacityweight.setAnchor('left', 'extraplanarcontents', 'right', 10) end
 	window.contentsvolume.setVisible(bVisible)
 	window.contentsvolume_label.setVisible(bVisible)
 	window.internal_dimensions_label.setVisible(bVisible)
